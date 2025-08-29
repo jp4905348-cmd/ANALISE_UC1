8437b2ed12115c2bb74839d5e54ff38aa5239aab
JOAO PEDRO GONCALVES RODRIGUES <joaog.rodrigues@aluno.senac.rio>
2025-08-28 21:44:02 -0300

    estruturas de repetição e range

---
+++ soma.py
@@ -0,0 +1,16 @@
+soma = 0
+#n = range(3)
+#print(n)
+for i in range (10):
+    n = int(input(f'informe {i+1}º numero:'))
+soma = soma + n
+print(f'a soma é:{soma}')
+
+
+"""""
+n1 = int(input('informe um numero: '))
+n2 = int(input('informe outro numero: '))
+n3 = int(input('informe mais um numero:'))
+soma = n1 + n2 + n3
+print(f'a soma é:{soma}')
+"""
